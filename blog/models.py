ddf761c4d96ea2ca358ef17422e83b00c3ecdd8f
AlexBulavin <bulavin1970@icloud.com>
2021-06-26 20:19:15 +0300

    Register model in DB and display DB elements natively. Like user named them.

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 class ArticleModel(models.Model):
+    #Создали мета класс для отображения заголовков элементов БД в пользовательской форме
+    class Meta:
+        ordering = ('creation_date',)
+        verbose_name = ('Article title')
+        verbose_name_plural = ('Article titles')
+
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     content = models.TextField()
     creation_date = models.DateTimeField()
+
+#Делаем отображение элементов (заголовков постов) в админке БД нативным, как назвал пользователь, а не в виде индексов БД
+    def __str__(self):
+        return self.title
